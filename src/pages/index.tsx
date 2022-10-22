f1d9b41df044f6701a4379f6e071777e0379372f
Diego Ramires <goncalves.ramires@gmail.com>
2022-10-22 14:50:40 -0300

    add: basic styles, custom font and static home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,46 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={styles.contentContainer}>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <title>
+            Como utilizar hooks
+          </title>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores</p>
+          <div>
+            <time>22 out 2022</time>
+            <span>Diego</span>
+          </div>
+        </div>
+
+        <div>
+          <title>
+            Como utilizar hooks
+          </title>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores</p>
+          <div>
+            <time>22 out 2022</time>
+            <span>Diego</span>
+          </div>
+        </div>
+
+        <div>
+          <title>
+            Como utilizar hooks
+          </title>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores</p>
+          <div>
+            <time>22 out 2022</time>
+            <span>Diego</span>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
